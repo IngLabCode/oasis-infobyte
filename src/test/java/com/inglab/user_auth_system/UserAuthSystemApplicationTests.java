8a778f46b131d02bba55df4c77f55fb1aedd01cc
Inqilab Aliyev <eliyevinqlab.02@gmail.com>
2024-12-07 17:30:09 +0400

    first commit

---
+++ src/test/java/com/inglab/user_auth_system/UserAuthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inglab.user_auth_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
